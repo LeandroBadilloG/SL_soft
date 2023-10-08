bbf3110b451e0183ad794cc18b620b727eca4ec0
Leandro <ljbadillo7@soy.sena.edu.co>
2023-10-08 11:05:16 -0500

    inicio de documentacion del endpoint

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -6,6 +6,47 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   productos:
+ *    type: object
+ *    properties:
+ *     nombre:
+ *      type: string
+ *      description: Se necesita un nombre para el producto
+ *     talla:
+ *      type: string
+ *      description: Se necesita un talla para el producto
+ *     referencia:
+ *      type: string
+ *      description: Se necesita una referencia para identificar el producto
+ *     cantidad:
+ *      type: number
+ *      description: Se necesita la cantidad de unidades disponibles
+ *     precio:
+ *      type: number
+ *      description: Se necesita el precio por unidiad
+ *     categoria:
+ *      type: string
+ *      description: Se necesita una categoria para agrupar los productos
+ *    required:
+ *     - nombre
+ *     - referencia
+ *     - cantidad
+ *     - precio
+ *     - descripcion
+ *     - categoria
+ *    example:
+ *     nombre: pantalon
+ *     talla: M
+ *     referencia: P008
+ *     cantidad: 50
+ *     precio: 100000
+ *     descripcion: El mejor pantalon que veras en tu vida
+ *     categoria: Pantalones
+ */
 router.get('/listarProducto', funcionesProductos.listarProductos);
 router.get('/eliminarProducto/:id', funcionesProductos.eliminarProducto);
 router.post('/agregarProducto', funcionesProductos.guardaProducto);
